ef3974a9932bc285363bec577093ece56476a6a6
melikbugra <melikubuntu@gmail.com>
2022-02-07 18:47:01 +0300

    State and space updated

--- simple_maze_env/envs/graphics/agent_renderer.py
+++ simple_maze_env/envs/graphics/agent_renderer.py
@@ -1,4 +1,6 @@
+from numpy import dtype
 import pygame
+import numpy as np
 
 
 class AgentRenderer:
@@ -7,35 +9,35 @@ class AgentRenderer:
         self.agent_image = agent_image
 
     def draw_agent(self, stt):
-        if stt == 0:
+        if list(map(int, stt)) == [0, 0]:
             self.win.blit(self.agent_image, (151.25, 51.25))
-        if stt == 1:
+        if list(map(int, stt)) == [0, 1]:
             self.win.blit(self.agent_image, (283.75, 51.25))
-        if stt == 2:
+        if list(map(int, stt)) == [0, 2]:
             self.win.blit(self.agent_image, (416.25, 51.25))
-        if stt == 3:
+        if list(map(int, stt)) == [0, 3]:
             self.win.blit(self.agent_image, (548.75, 51.25))
-        if stt == 4:
+        if list(map(int, stt)) == [1, 0]:
             self.win.blit(self.agent_image, (151.25, 183.75))
-        if stt == 5:
+        if list(map(int, stt)) == [1, 1]:
             self.win.blit(self.agent_image, (283.75, 183.75))
-        if stt == 6:
+        if list(map(int, stt)) == [1, 2]:
             self.win.blit(self.agent_image, (416.25, 183.75))
-        if stt == 7:
+        if list(map(int, stt)) == [1, 3]:
             self.win.blit(self.agent_image, (548.75, 183.75))
-        if stt == 8:
+        if list(map(int, stt)) == [2, 0]:
             self.win.blit(self.agent_image, (151.25, 316.25))
-        if stt == 9:
+        if list(map(int, stt)) == [2, 1]:
             self.win.blit(self.agent_image, (283.75, 316.25))
-        if stt == 10:
+        if list(map(int, stt)) == [2, 2]:
             self.win.blit(self.agent_image, (416.25, 316.25))
-        if stt == 11:
+        if list(map(int, stt)) == [2, 3]:
             self.win.blit(self.agent_image, (548.75, 316.25))
-        if stt == 12:
+        if list(map(int, stt)) == [3, 0]:
             self.win.blit(self.agent_image, (151.25, 448.75))
-        if stt == 13:
+        if list(map(int, stt)) == [3, 1]:
             self.win.blit(self.agent_image, (283.75, 316.25))
-        if stt == 14:
+        if list(map(int, stt)) == [3, 2]:
             self.win.blit(self.agent_image, (416.25, 316.25))
-        if stt == 15:
+        if list(map(int, stt)) == [3, 3]:
             self.win.blit(self.agent_image, (548.75, 316.25))
--- simple_maze_env/envs/simple_maze_env.py
+++ simple_maze_env/envs/simple_maze_env.py
@@ -4,6 +4,7 @@ from gym.utils import seeding
 import time
 import sys
 from simple_maze_env.envs.graphics.graphics import VisualRenderer
+import numpy as np
 
 class SimpleMazeEnv(gym.Env):
     metadata = {'render.modes': ['cli', 'graphics']}
@@ -11,12 +12,16 @@ class SimpleMazeEnv(gym.Env):
     def __init__(self):
         self.safe_path = [(0, 0), (0, 1), (0, 2), (0, 3),
                           (1, 3), (2, 3), (2, 2), (2, 1), (2, 0), (3, 0)]
-        self.cliffs = [(1, 0), (1, 1), (1, 2), (3, 1), (3, 2), (3, 3)]
+        self.cliffs = [(1, 0), (1, 1), (1, 2), (3, 1), (3, 2), (3, 3)],
+        low = np.array([0, 0], dtype=np.float32)
+        high = np.array([3, 3], dtype=np.float32)
+        self.action_space = spaces.Discrete(4)
+        self.observation_space = spaces.Box(low, high, dtype=np.float32)
         self.start_loc = self.safe_path[0]
         self.trm_loc = self.safe_path[9]
         self.agent_loc = ()
         self.num_states = len(self.safe_path) + len(self.cliffs)
-        self.state = 0
+        self.state = self.observation_space.low
         self.reward = 0
         self.done = False
         self.info = "Info"
@@ -34,51 +39,51 @@ class SimpleMazeEnv(gym.Env):
             if possible_next_loc[0] >= 0:  # check the agent within grid?
                 # if within grid, current location is possible location
                 self.agent_loc = possible_next_loc
-            else:
-                self.agent_loc = (1, 0)  # go to a cliff
+            # else:
+            #     self.agent_loc = (1, 0)  # go to a cliff
         elif action == 1:  # left
             self.temp_loc = self.agent_loc
             possible_next_loc = (self.agent_loc[0], self.agent_loc[1] - 1)
             if possible_next_loc[1] >= 0:  # check the agent within grid?
                 # if within grid, current location is possible location
                 self.agent_loc = possible_next_loc
-            else:
-                self.agent_loc = (1, 0)  # go to a cliff
+            # else:
+            #     self.agent_loc = (1, 0)  # go to a cliff
         elif action == 2:  # down
             self.temp_loc = self.agent_loc
             possible_next_loc = (self.agent_loc[0] + 1, self.agent_loc[1])
             if possible_next_loc[0] < 4:  # check the agent within grid?
                 # if within grid, current location is possible location
                 self.agent_loc = possible_next_loc
-            else:
-                self.agent_loc = (1, 0)  # go to a cliff
+            # else:
+            #     self.agent_loc = (1, 0)  # go to a cliff
         elif action == 3:  # right
             self.temp_loc = self.agent_loc
             possible_next_loc = (self.agent_loc[0], self.agent_loc[1] + 1)
             if possible_next_loc[1] < 4:  # check the agent within grid?
                 # if within grid, current location is possible location
                 self.agent_loc = possible_next_loc
-            else:
-                self.agent_loc = (1, 0)  # go to a cliff
+            # else:
+            #     self.agent_loc = (1, 0)  # go to a cliff
 
-        self.reward = 0  # initiate self.reward as -1,agent gets a -1 self.reward for its every step in safe path,so it does not stuck
+        self.reward = -1  # initiate self.reward as -1,agent gets a -1 self.reward for its every step in safe path,so it does not stuck
         self.done = False  # holds the termination information
 
         if self.agent_loc == self.trm_loc:  # check the self.done is reached?
-            self.reward = 1  # give 100 self.reward if it terminates the episode
+            self.reward = 100  # give 100 self.reward if it terminates the episode
             self.done = True  # bool of termination is true
             
         # check fell into cliffs?
         elif self.agent_loc in self.cliffs or (possible_next_loc in self.cliffs or possible_next_loc not in self.safe_path):
-            # self.reward = -10  # give -100 self.reward if it falls into the cliffs
-            self.done = False  # bool of termination is true
+            self.reward = -100  # give -100 self.reward if it falls into the cliffs
+            self.done = True  # bool of termination is true
             self.agent_loc = self.start_loc  # agent is sent to start state
             
         else:
-            self.reward = 0  # default value
+            self.reward = -1  # default value
             self.done = False  # default value
 
-        self.state = self.get_state_no(self.agent_loc)
+        self.state = np.array(self.agent_loc, dtype=np.float32)
 
         return [self.state, self.reward, self.done, self.info]
 
@@ -87,9 +92,9 @@ class SimpleMazeEnv(gym.Env):
         # agentLoc is the current location of the agent
         self.agent_loc = self.start_loc
         # state is state number as an int
-        self.state = self.get_state_no(self.agent_loc)
+        self.state = np.array(self.agent_loc, dtype=np.float32)
         self.done = False
-        return [self.state, self.reward, self.done, self.info]
+        return self.state
 
     def render(self, mode='cli', close=False):
         if mode=='cli':
